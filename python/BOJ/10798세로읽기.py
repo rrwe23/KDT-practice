581ce0422e3eeb07ac8d451dcd46c1e3432e40e9
rrwe23 <jhrrt0626@gmail.com>
2022-09-05 22:15:10 +0900

    0905 practice

---
+++ python/BOJ/10798세로읽기.py
@@ -0,0 +1,15 @@
+#https://www.acmicpc.net/problem/10798
+
+
+words = []
+length = []
+for _ in range(5):
+    word= input()
+    words.append(word)
+    length.append(len(word))
+
+rst = ''
+for i in range(max(length)):
+    for j in range(5):
+        if i < length[j]:
+            rst += words[j][i]
