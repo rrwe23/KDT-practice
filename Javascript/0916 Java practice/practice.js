a7818e76cb58da85d8f655c806a7067318a6fdaa
rrwe23 <jhrrt0626@gmail.com>
2022-09-20 00:28:30 +0900

    0919 JS

---
+++ Javascript/0916 Java practice/practice.js
@@ -0,0 +1,15 @@
+
+const numone = 5
+const numtwo = 10
+
+if (operator ==='+') {
+    console.log(numone + numtwo)
+}   else if (operator ==='-') {
+    console.log(numone - numtwo)
+}   else if (operator ==='*') {
+    console.log(numone * numtwo)
+}   else if (operator ==='/') {
+    console.log(numone / numtwo)
+}   else {
+    console.log('유효하지 않은 연산자입니다.')
+}
