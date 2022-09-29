8531c5d403754941f62cc516dc2d3ef401462151
Nabicat <jhrrt0626@gmail.com>
2022-09-30 01:13:03 +0900

    Update settings.py

--- Django/0929 django practice/0929pjt/project/settings.py
+++ Django/0929 django practice/0929pjt/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!jjh)-pbx2h5j*p4gvdtur^y$qa9kbur6(etu1$9op-^wf4wmk'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
