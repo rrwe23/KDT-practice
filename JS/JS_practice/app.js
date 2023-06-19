ac46109dd88f1c00e2bfdf0ea7cdad2afafbe85e
rrwe23 <jhrrt0626@gmail.com>
2023-06-19 22:52:38 +0900

    js0619

---
+++ JS/JS_practice/app.js
@@ -0,0 +1,7 @@
+const age = parseInt(prompt("How old are you?"));  // prompt는 자바스크립트를 멈추게 함, 그래서 잘 안씀
+
+// console.log(typeof age)  // typeof를 통해 형확인
+
+// console.log(typeof "15", typeof parseInt("15"))  // parseInt = 정수형변환
+
+console.log(age);
